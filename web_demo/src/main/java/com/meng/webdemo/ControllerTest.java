c3b4974d54806ecfadce66050c85223cd70145c4
wp <wp@wooduan.com>
2018-09-04 14:41:41 +0800

    rename

--- web_demo/src/main/java/com/meng/webdemo/ControllerTest.java
+++ web_demo/src/main/java/com/meng/webdemo/ControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.web_demo;
+package com.meng.webdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
--- web_demo/src/main/java/com/meng/webdemo/WebDemoApplication.java
+++ web_demo/src/main/java/com/meng/webdemo/WebDemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.web_demo;
+package com.meng.webdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
